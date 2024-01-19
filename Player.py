89cdbb49805b15b24a60541e7d44fd7d89dac901
Łukasz Chlebek <chlebeke.lukasz@gmail.com>
2024-01-19 22:08:48 +0100

    fix

--- Player.py
+++ Player.py
@@ -377,7 +377,7 @@ class Player:
                     direction_to_shoot = (enemy_position - self.Position_in_game).normalize()
                     angle = math.atan2(direction_to_shoot.y, direction_to_shoot.x)
                     #angle += math.radians(random.randint(-5, 5))
-                    #globals.projectiles.append(Projectile(self.map.WORLD, self.Position_in_game.x, self.Position_in_game.y, angle,self, True,self.color))
+                    globals.projectiles.append(Projectile(self.map.WORLD, self.Position_in_game.x, self.Position_in_game.y, angle,self, True,self.color))
                     self.railgun_ammo -= 1
                     self.is_casting_primary = False
                     self.last_time_shoot = timer
